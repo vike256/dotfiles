ca5de3017bfdd56c2373c27fb0bcb5a042ea1238
vike <69812383+vike256@users.noreply.github.com>
2026-07-04 16:37:35 +0300

    init

---
+++ .config/nvim/init.lua
@@ -0,0 +1,25 @@
+-- Enable line numbers
+vim.o.number = true
+
+-- Disable line wrapping
+vim.o.wrap = false
+
+-- Adjust the width of the tab character
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+-- Ignore case when the search pattern is all lowercase
+vim.o.smartcase = true
+vim.o.ignorecase = true
+
+-- Clear search highlights after submit
+vim.o.hlsearch = false
+
+-- Reserve a space in the gutter for signs. Some plugins use this to show icons.
+vim.o.signcolumn = 'yes'
+
+-- Use system clipboard
+--vim.o.clipboard = 'unnamedplus'
+
+-- vim.g.mapleader = vim.keycode('<Space>')
+
